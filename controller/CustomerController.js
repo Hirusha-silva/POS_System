22c3f50f3821c38206659a0adc0762d4bce829ec
Hirusha-silva <hirushasilva222@gmail.com>
2025-05-07 00:18:18 +0530

    load customer table data

--- controller/CustomerController.js
+++ controller/CustomerController.js
@@ -4,6 +4,7 @@ import CustomerModel from "../model/CustomerModel.js";
 // load customer Id
 $(document).ready(function (){
    $('#customerId').val(generateCustomerId());
+   loadCustomer();
 });
 
 // generate customer Id
@@ -15,4 +16,26 @@ function generateCustomerId(){
    let numberPart = parseInt(lastId.substring(1));
    let newId = numberPart + 1;
    return "C" + newId.toString().padStart(3,'0');
+}
+
+// load table data
+function loadCustomer() {
+   $('#customer_tbody').empty();
+   customer_db.map((customer,index) => {
+      let customerId = customer.customerId;
+      let name = customer.name;
+      let address = customer.address;
+      let email = customer.email;
+      let number = customer.number;
+
+      let data = `<tr>
+                            <td>${customerId}</td>
+                            <td>${name}</td>
+                            <td>${address}</td>
+                            <td>${email}</td>
+                            <td>${number}</td>
+                        </tr>`
+
+      $('#customer_tbody').append(data);
+   });
 }
