1f6b270e03bbcd51c499ce5ba2aa67e95e88c940
Hirusha-silva <hirushasilva222@gmail.com>
2025-05-07 23:12:57 +0530

    create item model

---
+++ model/ItemModel.js
@@ -0,0 +1,8 @@
+export default class ItemModel {
+    constructor(item_id,item_name,qty,price) {
+        this.item_Id = item_id;
+        this.item_name = item_name;
+        this.qty = qty;
+        this.price = price;
+    }
+}
