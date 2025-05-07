32232cab8c42a13e5992eb894190f9d8a3d6d981
Hirusha-silva <hirushasilva222@gmail.com>
2025-05-07 23:24:59 +0530

    generate item id

---
+++ controller/ItemController.js
@@ -0,0 +1,18 @@
+import {item_db} from "../db/db.js";
+import ItemModel from "../model/ItemModel.js";
+
+// load item Id
+// $(document).ready(function (){
+//    $('item_id').val()
+// });
+
+// generate item Id
+function generateItemId(){
+    if (item_db.length === 0){
+        return 'I001';
+    }
+    let lastId = item_db[item_db.length -1].item_Id;
+    let numberPart = parseInt(lastId.substring(1));
+    let newId = numberPart + 1;
+    return "I" + newId.toString().padStart(3,'0');
+}
